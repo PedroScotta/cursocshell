f71f33ddaa8da11c6ecb42549ba0d6f5b9f00ea8
Pedro Scottá <pedro.scotta@gmail.com>
2022-02-15 16:33:03 -0300

    Projeto de aula C#.

---
+++ Aluguel/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aluguel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistroDeAluguel[] vet = new RegistroDeAluguel[10];
+            string nome, email;
+            int quarto;
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Aluguel #" + (i + 1) + ":");
+                Console.Write("Nome: ");
+                nome = Console.ReadLine();
+                Console.Write("Email: ");
+                email = Console.ReadLine();
+                Console.Write("Quarto? ");
+                quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new RegistroDeAluguel (nome, email);
+                Console.WriteLine();
+            }
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0;i < 10;i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i + ", " + vet[i]);
+                }
+            }
+        }
+    }
+}
---
+++ Aluguel/RegistroDeAluguel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aluguel
+{
+    class RegistroDeAluguel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public RegistroDeAluguel(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return Nome + ", " + Email;                    
+        }
+    }
+}
